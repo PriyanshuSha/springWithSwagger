cc3416b8920002ee14caefb5399648612eed366f
PriyanshuSha <priyanshusharma90284@gmail.com>
2022-11-03 17:26:04 +0530

    security added

--- src/main/java/com/Student/SpringDataValidationApplication.java
+++ src/main/java/com/Student/SpringDataValidationApplication.java
@@ -2,12 +2,14 @@ package com.Student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
 @EnableSwagger2
 @SpringBootApplication
 public class SpringDataValidationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataValidationApplication.class, args);
 	}
